ff11a5a4a723bb4e6cc178c4a1b1cd9198931e7b
ludwigwermke <ludwig.wermke@mementor.de>
2022-06-25 09:39:11 +0200

    remove unnecessary service

--- projects/ngx-date-bar-chart/src/public-api.ts
+++ projects/ngx-date-bar-chart/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-date-bar-chart
  */
 
-export * from './lib/ngx-date-bar-chart.service';
 export * from './lib/ngx-date-bar-chart.component';
 export * from './lib/ngx-date-bar-chart.module';
