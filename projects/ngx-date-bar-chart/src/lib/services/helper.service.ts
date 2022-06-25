9312828a9f0d22055d44589dc8b62339ff9041a1
ludwigwermke <ludwig.wermke@mementor.de>
2022-06-25 10:32:27 +0200

    add prettier and format files

--- projects/ngx-date-bar-chart/src/lib/services/helper.service.ts
+++ projects/ngx-date-bar-chart/src/lib/services/helper.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
-import {INgxDateValue} from '../interfaces/date-value.interface';
+import { INgxDateValue } from '../interfaces/date-value.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HelperService {
-
-  constructor() { }
+  constructor() {}
 
   public getXDomain(processedData: INgxDateValue[]): [Date, Date] {
     if (!processedData?.length) {
-      throw new RangeError('needs at least one value to properly set up chart.');
+      throw new RangeError(
+        'needs at least one value to properly set up chart.'
+      );
     }
-    return [processedData[0].date, processedData[processedData.length-1].date];
+    return [
+      processedData[0].date,
+      processedData[processedData.length - 1].date,
+    ];
   }
 
   public getYDomain(processedData: INgxDateValue[]): [number, number] {
     if (!processedData?.length) {
-      throw new RangeError('needs at least one value to properly set up chart.');
+      throw new RangeError(
+        'needs at least one value to properly set up chart.'
+      );
     }
-    return [processedData[0].value, processedData[processedData.length-1].value];
+    return [
+      processedData[0].value,
+      processedData[processedData.length - 1].value,
+    ];
   }
 }
