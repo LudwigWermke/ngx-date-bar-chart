00bc605de5520314928b33e75343b469be4eba31
ludwigwermke <ludwig.wermke@mementor.de>
2022-06-25 09:55:33 +0200

    add input data in demo project

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { INgxDateValue } from 'projects/ngx-date-bar-chart/src/lib/interfaces/date-value.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  public data: INgxDateValue[] = [];
+
+  constructor() {
+    const data: INgxDateValue[] = [];
+    for (let i = 0; i < 10; ++i) {
+      const date = new Date();
+      date.setDate(new Date().getDate() + i);
+
+      data.push({
+        date, value: i * i
+      })
+    }
+
+    this.data = data;
+  }
 }
