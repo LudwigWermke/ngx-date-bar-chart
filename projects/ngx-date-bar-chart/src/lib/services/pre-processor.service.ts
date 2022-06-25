5c9f0807372675e869e3bb044effaf8e05413b5c
ludwigwermke <ludwig.wermke@posteo.de>
2022-06-25 11:59:54 +0200

    add error to preprocessing service

--- projects/ngx-date-bar-chart/src/lib/services/pre-processor.service.ts
+++ projects/ngx-date-bar-chart/src/lib/services/pre-processor.service.ts
@@ -8,6 +8,11 @@ export class PreProcessorService {
   constructor() {}
 
   public preProcess(data: INgxDateValue[]): INgxDateValue[] {
+    if (!data?.length) {
+      throw new RangeError(
+        'ngx-date-bar-chart needs at least one entry as an input'
+      );
+    }
     const processedData = data.map((entry) => {
       const date = entry.date;
       date.setHours(0, 0, 0, 0);
