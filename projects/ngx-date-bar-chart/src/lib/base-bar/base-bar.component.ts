458a3fbf3e9f67569bb903ff9d4213d4a215bce4
ludwigwermke <ludwig.wermke@mementor.de>
2022-07-23 22:01:35 +0200

    use cli to generate bar

---
+++ projects/ngx-date-bar-chart/src/lib/base-bar/base-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'base-bar',
+  templateUrl: './base-bar.component.html',
+  styleUrls: ['./base-bar.component.css']
+})
+export class BaseBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
