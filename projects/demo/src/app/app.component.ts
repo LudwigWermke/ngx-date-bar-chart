9ce79763775528ee07a489c4a680c673611d597d
ludwigwermke <ludwig.wermke@mementor.de>
2022-07-24 19:30:37 +0200

    add further customization

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { INgxDateValue } from 'projects/ngx-date-bar-chart/src/lib/interfaces/date-value.interface';
 import { INgxDateValueSeries } from '../../../ngx-date-bar-chart/src/lib/interfaces/date-value-series.interface';
+import { NgxDateBarChartComponent } from 'ngx-date-bar-chart';
+import { LegendPosition } from 'projects/ngx-date-bar-chart/src/lib/enums/legend-position.enum';
 
 @Component({
   selector: 'app-root',
@@ -8,40 +10,27 @@ import { INgxDateValueSeries } from '../../../ngx-date-bar-chart/src/lib/interfa
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // // TODO: api ref
-  // @ViewChildren(NgxDateBarChartComponent)
-  // children!: QueryList<NgxDateBarChartComponent>;
+  /**
+   * use this to manually trigger resizing a component
+   * if you have to (resizing gets triggered  only on window:resize())
+   * and just call 'resize()' for each child/chart e.g.
+   * `this.children.forEach((c) => c.resize());`
+   */
+  @ViewChildren(NgxDateBarChartComponent)
+  children!: QueryList<NgxDateBarChartComponent>;
 
-  public data: INgxDateValueSeries[] = [];
   public basicData: INgxDateValue[] = [];
   public basicSeriesData: INgxDateValueSeries[] = [];
+
+  public LegendPosition = LegendPosition;
 
   constructor() {
     this.initBasicData();
     this.initBasicSeriesData();
-    const data: INgxDateValueSeries[] = [];
-    // const data: INgxDateValue[] = [];
-    for (let i = 0; i < 10; ++i) {
-      const date = new Date();
-      date.setDate(new Date().getDate() + i);
-
-      data.push({
-        date,
-        values: [i, i, 3 * i, 2 * i],
-        // value: i * i,
-      });
-    }
-
-    this.data = data;
-
-    // TODO: include in docs
-    // setTimeout(() => {
-    //   this.children.forEach((c) => c.resize());
-    // }, 5000);
   }
 
   public barRadiusFunction(barWidth: number): number {
-    return barWidth / 3;
+    return barWidth / 6;
   }
 
   private initBasicData(): void {
@@ -59,8 +48,13 @@ export class AppComponent {
     for (let i = 0; i < 10; ++i) {
       const date = new Date();
       date.setDate(date.getDate() + i);
-      data.push({ date, values: [5 + i * i, 10 + i * i, 5 * i] });
+      data.push({ date, values: [50 + 5 + i * i, 60 + i * i, 5 * i + 30] });
     }
     this.basicSeriesData = data;
+  }
+
+  public formatDate(date: Date): string {
+    const options: any = { month: '2-digit', day: '2-digit', year: '2-digit' };
+    return date.toLocaleDateString('de-DE', options);
   }
 }
