e562c6f84b6d8b0b6531d797e520b044243c2b26
oemer-mutluel <oemer.mutluel@mementor.de>
2022-07-31 11:23:59 +0300

    add stuff to package json and add exports to public api

--- projects/ngx-date-bar-chart/src/public-api.ts
+++ projects/ngx-date-bar-chart/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/ngx-date-bar-chart.component';
 export * from './lib/ngx-date-bar-chart.module';
+export * from './lib/enums/legend-position.enum';
+export * from './lib/interfaces/date-value-series.interface';
+export * from './lib/interfaces/date-value.interface';
